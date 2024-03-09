0ca898a2e038e5e8ee4c20250464f818827f0f83
RaeesaParker <raeesaparker101@gmail.com>
2024-03-09 17:54:42 +0000

    Add homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box } from '@chakra-ui/react';
+import { useMemo } from 'react';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import { webRoutes } from "./routes";
 
 function App() {
+
+  const router = useMemo(() => {
+		return createBrowserRouter([...webRoutes],);
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <RouterProvider router={router} />
+    </Box>
   );
 }
 
---
+++ src/routes.tsx
@@ -0,0 +1,5 @@
+import { Homepage } from "./pages/Homepage";
+
+export const webRoutes = [
+  { path: "/", element: ( < Homepage/> ) }, 
+];
